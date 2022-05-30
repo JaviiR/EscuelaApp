897e6d90aa5b72c980238269510916440cbe7e55
JaviiR <alexircaa@gmail.com>
2022-05-30 17:35:40 -0500

    Cambio nombre variable servicio --> servicioV

--- src/main/java/com/vila/controller/TablasController.java
+++ src/main/java/com/vila/controller/TablasController.java
@@ -18,34 +18,33 @@ import com.vila.services.IVacantesService;
 public class TablasController {
 
 	@Autowired
-	private IVacantesService servicio;
-
-Separador s=new Separador();
+	private IVacantesService servicioV;
+	Separador s=new Separador();
 	
 
 	@GetMapping("Lista/alumnos")//METODO QUE LISTA TODOS LOS ALUMNOS DE LA BASE DE DATOS XD
 	public String MostrarAlumnos(Model model) {
-		List<Alumno> alumnos = servicio.buscarTodosAlumnos();
+		List<Alumno> alumnos = servicioV.buscarTodosAlumnos();
 		model.addAttribute("alumnos", alumnos);
 		return "alumnos";
 	}
 
 	@GetMapping("Lista/profesores")//METODO QUE LISTA TODOS LOS PROFESORES DE LA BASE DE DATOS XD
 	public String MostrarProfesores(Model model) {
-		List<Profesor> profes = servicio.buscartodosProfesores();
+		List<Profesor> profes = servicioV.buscartodosProfesores();
 		model.addAttribute("profes", profes);
 		return "listaProfesores";
 	}
 	
 	@GetMapping("Lista/alumnos/filtro")//METODO QUE LISTA UN ALUMNO EN ESPECIFICADO SEGUN EL NOMBRE
 	public String FiltrarAlumno(Model model,@RequestParam String nombre) {
-			model.addAttribute("alumnos", servicio.buscarAlumno(nombre));
+			model.addAttribute("alumnos", servicioV.buscarAlumno(nombre));
 			return "alumnos";
 	}
 	
 	@GetMapping("Lista/profesores/filtro")//METODO QUE LISTA UN PROFESOR EN ESPECIFICO SEGUN EL NOMBRE
 	public String FiltrarProfesor(Model model,@RequestParam String nombre) {
-		model.addAttribute("profes", servicio.buscarProfesor(nombre));
+		model.addAttribute("profes", servicioV.buscarProfesor(nombre));
 		return "listaProfesores";
 	}
 	
